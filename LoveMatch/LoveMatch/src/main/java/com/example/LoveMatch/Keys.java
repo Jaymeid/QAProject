951f74955eb1bcc38b7e8e44f709ac2e254ceef3
Jaymeid <jamespawson64@gmail.com>
2023-10-12 16:09:47 +0100

    Modified project gitignore

--- LoveMatch/LoveMatch/src/main/java/com/example/LoveMatch/Keys.java
+++ LoveMatch/LoveMatch/src/main/java/com/example/LoveMatch/Keys.java
@@ -1,7 +1,7 @@
 package com.example.LoveMatch;
 
 public class Keys {
-    static String OPENAIKEY = "This shouldnt appear";
+    static String OPENAIKEY = "This REALLY shouldnt appear";
 
     public static String getOPENAIKEY(){
         return OPENAIKEY;
