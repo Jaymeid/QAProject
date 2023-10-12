f153b6a88b7b7bc7de07dbf34f1bb925226bfc81
Jaymeid <jamespawson64@gmail.com>
2023-10-12 16:18:24 +0100

    Deleted keys.java

--- LoveMatch/LoveMatch/src/main/java/com/example/LoveMatch/Keys.java
+++
@@ -1,9 +0,0 @@
-package com.example.LoveMatch;
-
-public class Keys {
-    static String OPENAIKEY = "Please don't appear";
-
-    public static String getOPENAIKEY(){
-        return OPENAIKEY;
-    }
-}
