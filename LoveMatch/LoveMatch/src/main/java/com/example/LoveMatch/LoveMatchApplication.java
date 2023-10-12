a69b47a0a51f34cdb862764d8d9c12d25f01d35c
Jaymeid <56159890+Jaymeid@users.noreply.github.com>
2023-10-12 10:18:19 +0100

    Springboot Init files

---
+++ LoveMatch/LoveMatch/src/main/java/com/example/LoveMatch/LoveMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoveMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveMatchApplication.class, args);
+	}
+
+}
