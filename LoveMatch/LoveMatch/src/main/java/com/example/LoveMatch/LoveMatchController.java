35b0ca63d953f696f87edd5532103aa164b39513
jyo <jyothsna.sambasivam@qa-training.net>
2023-10-12 12:06:23 +0100

    Mapping Percentage

--- LoveMatch/LoveMatch/src/main/java/com/example/LoveMatch/LoveMatchController.java
+++ LoveMatch/LoveMatch/src/main/java/com/example/LoveMatch/LoveMatchController.java
@@ -1,6 +1,7 @@
 package com.example.LoveMatch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -9,29 +10,18 @@ import java.util.Scanner;
 public class LoveMatchController {
     private int loveMatchPercentage;
 
+
     public LoveMatchController(){
         Random random = new Random();
         loveMatchPercentage = random.nextInt(101);
     }
 
-    public void generateLoveMatch() {
-        //MVP example
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the LoveMatch Generator");
-        System.out.println("Enter First Name");
-        String name1 = scanner.nextLine();
-
-        System.out.println("Enter Second Name");
-        String name2 = scanner.nextLine();
-
+    @GetMapping
+    public  Integer generateLoveMatch() {
         Random random = new Random();
         loveMatchPercentage = random.nextInt(101);
-
-        System.out.println("The match between " + name1 + " and " + name2 + " is " + loveMatchPercentage + "%");
-    }
-
-    public int getLoveMatchPercentage() {
         return loveMatchPercentage;
     }
+
+
 }
